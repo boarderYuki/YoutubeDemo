826f3c1abe9ba34ad334c45d3ee4bf82723a59a3
yuki.pro <yuki.kr@gmail.com>
2018-07-04 14:29:58 +0900

    no message

--- YoutubeDemo/ViewController.swift
+++ YoutubeDemo/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate{
 
+    @IBOutlet var videoView: WKWebView!
+    
+    @IBOutlet weak var videoView2: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        videoView.uiDelegate = self
+        videoView.navigationDelegate = self
+        
+        // WK웹뷰
+        let initURL = "http://dh2y.iptime.org:8081"
+        let myURL = URL(string: initURL)
+        let myRequest = URLRequest(url: myURL!)
+        videoView?.load(myRequest)
+        
+        
+        
+//
+////        let url = Bundle.main.url(forResource: "index", withExtension: "html")!
+//                let initURL = "http://dh2y.iptime.org:8081/login/login.do"
+//                let url = URL(string: initURL)
+//        //videoView.loadFileURL(url!, allowingReadAccessTo: url!)
+//
+//        videoView.loadHTMLString(initURL, baseURL: nil)
+////        let request = URLRequest(url: url!)
+////        videoView.load(request)
+        
+        
+        
+        
+        
+        // UI웹뷰
+        videoView2.allowsInlineMediaPlayback = true
+//        videoView2.loadHTMLString("<iframe width=\"\(videoView2.frame.width)\" height=\"\(videoView2.frame.height)\" src=\"http://dh2y.iptime.org:8081\" frameborder=\"0\" allowfullscreen></iframe>", baseURL: nil)
+//        videoView2.loadHTMLString("<iframe  width=\"\(videoView2.frame.width)\" height=\"\(videoView2.frame.height)\" src=\"http://www.youtube.com/embed/9by4H4khSFg\"></iframe>", baseURL: nil)
+     //<iframe id="player" type="text/html" width="640" height="360"  src="http://www.youtube.com/embed/9by4H4khSFg" ></iframe>
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
